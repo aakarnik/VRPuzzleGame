91ccc099806cfc30c4040a840d6a1ed37a4f05e0
Anuya Karnik <aakarnik8@gmail.com>
2019-04-23 00:49:02 -0400

    Added new script for Puzzle1 (TODO), Updated Readme.md

---
+++ VRPuzzle/Assets/Scripts/Puzzle2Controller.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle2Controller : MonoBehaviour
+{
+
+    //The height at which the blocks can rest on the base 
+    public float tolerance;
+
+    //Base of the puzzle
+    private GameObject mBrownBlock;
+
+    //TODO: Create one private GameObject variable named "puzzle1block"
+
+
+    /** TODO: Create one public int variable named "blockNum" to store the number of the block number 
+               This will be helpful to retrieve each block */
+
+
+
+
+    private float mBrownHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     /** TODO: Assign a tag name to the BrownBlock in Unity 
+               Set "mBrownBlock" equal to the object with the Brownblock tag  
+              (see PuzzlePlacer.cs line 22 for reference) */
+
+
+
+     /** TODO: Assign a consistent numbered tag to every puzzle block for Puzzle 1 (block1, block2, block3, etc.)
+               Set "puzzle1block" equal to the object with the tag string and the "blockNum" variable 
+              (see PuzzlePlacer.cs line 23 for reference)*/
+
+
+
+
+        
+        mBrownHeight = mBrownBlock.GetComponent<Renderer>().bounds.size.y; 
+
+    }
+
+    // Return true if yellow block is on top of brown block
+    bool IsSolved()
+    {
+        /** TODO: Create a Vector3 variable name "BlockPosition"
+         *        Store the position of the "puzzle1block" in it (just like BrownPosition)
+         */
+
+        //Storing the position of the Brown Base Block
+        Vector3 BrownPosition = mBrownBlock.transform.position;
+
+        /** TODO: Complete the if-statement below
+         *        Use "BlockPosition" instead of "YellowPosition"
+         *        Now the code should work for all blocks instead of just the Yellow Block
+         */
+
+        //if (YellowPosition.y > BrownPosition.y
+        //&& (YellowPosition.y - BrownPosition.y) < mBrownHeight + tolerance
+        //&& Mathf.Abs(YellowPosition.x - BrownPosition.x) < tolerance
+        //&& Mathf.Abs(YellowPosition.z - BrownPosition.z) < tolerance)
+        //{
+               //return true;
+         // }
+
+
+
+        //currently the function will only return false
+        return false;
+    }
+    void Update()
+    {
+        if (IsSolved() == true)
+        {
+            // Puzzle has been solved. Turn blocks white.
+
+
+            /*TODO: Change the color of the "Puzzle1Block" to white
+                    Puzzle1Block.GetComponent<Renderer>().material.color = Color.white;*/
+
+        }
+
+        // Add this script to the BrownBlock
+        // Test on Oculus to see if this functions so far
+    }
+}
+ 
--- readme.md
+++ readme.md
@@ -1,4 +1,20 @@
-This is the VR Project
+# VR Puzzle Game
 
-COntributors: Martha, Anuya.
+## Description: A simple, two level puzzle game which allows players to pick up and place objects, use dials, etc. to complete and win the game. 
+
+## Contributors: Martha Brandt, Anuya Karnik
+
+### Features to be added:
+
+[x] Room1 Backdrop and Furniture - M
+[x] Room2 Backdrop and Furniture - A
+[x] Create Puzzle 1 - M
+[ ] Code for Puzzle 1 - M
+[x] Create Puzzle 2 - A
+[x] Code for Puzzle 2 - A
+[ ] Complete Puzzle 1 to create a Dial that moves Wall - M
+[ ] Complete Puzzle 2 to create a Key that opens a Box - A
+[ ] Create a Box that opens and reveals the final prize - A
+[ ] Declare that Game is won after final prize is picked up - A
+
 
