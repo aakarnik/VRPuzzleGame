8e4699ecdeec300fdb79c43d6e5ab12f44d5b08b
Martha Brandt <brandt@inside.andrews.edu>
2019-04-23 17:01:06 -0400

    tester 7

--- VRPuzzle/Assets/Scripts/Puzzle2Controller.cs
+++ VRPuzzle/Assets/Scripts/Puzzle2Controller.cs
@@ -11,35 +11,44 @@ public class Puzzle2Controller : MonoBehaviour
     //Base of the puzzle
     private GameObject mBrownBlock;
 
-    //TODO: Create one private GameObject variable named "puzzle1block"
+    //x position
+    public float xconstraint1;
+    public float xconstraint2;
 
+    //y position
+    public float zconstraint1;
+    public float zconstraint2;
+
+    //color
+    private Color blockColor;
+
+    //TODO: Create one private GameObject variable named "puzzle1block"
+    private GameObject puzzle1block;
 
     /** TODO: Create one public int variable named "blockNum" to store the number of the block number 
                This will be helpful to retrieve each block */
 
-
-
+    public int blockNum = 0;
 
     private float mBrownHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-     /** TODO: Assign a tag name to the BrownBlock in Unity 
-               Set "mBrownBlock" equal to the object with the Brownblock tag  
-              (see PuzzlePlacer.cs line 22 for reference) */
+        /** TODO: Assign a tag name to the BrownBlock in Unity 
+                  Set "mBrownBlock" equal to the object with the Brownblock tag  
+                 (see PuzzlePlacer.cs line 22 for reference) */
+        mBrownBlock = GameObject.FindGameObjectWithTag("mBrownBlock").gameObject;
 
+        /** TODO: Assign a consistent numbered tag to every puzzle block for Puzzle 1 (block1, block2, block3, etc.)
+                  Set "puzzle1block" equal to the object with the tag string and the "blockNum" variable 
+                 (see PuzzlePlacer.cs line 23 for reference)*/
+        puzzle1block = GameObject.FindGameObjectWithTag("block" + blockNum).gameObject;
 
+        mBrownHeight = mBrownBlock.GetComponent<Renderer>().bounds.size.y;
 
-     /** TODO: Assign a consistent numbered tag to every puzzle block for Puzzle 1 (block1, block2, block3, etc.)
-               Set "puzzle1block" equal to the object with the tag string and the "blockNum" variable 
-              (see PuzzlePlacer.cs line 23 for reference)*/
-
-
-
-
-        
-        mBrownHeight = mBrownBlock.GetComponent<Renderer>().bounds.size.y; 
+        //Retrieve block color
+        //blockColor = puzzle1block.Shader.Find("_color");
 
     }
 
@@ -50,6 +59,8 @@ public class Puzzle2Controller : MonoBehaviour
          *        Store the position of the "puzzle1block" in it (just like BrownPosition)
          */
 
+        Vector3 BlockPosition = puzzle1block.transform.position;
+
         //Storing the position of the Brown Base Block
         Vector3 BrownPosition = mBrownBlock.transform.position;
 
@@ -58,17 +69,13 @@ public class Puzzle2Controller : MonoBehaviour
          *        Now the code should work for all blocks instead of just the Yellow Block
          */
 
-        //if (YellowPosition.y > BrownPosition.y
-        //&& (YellowPosition.y - BrownPosition.y) < mBrownHeight + tolerance
-        //&& Mathf.Abs(YellowPosition.x - BrownPosition.x) < tolerance
-        //&& Mathf.Abs(YellowPosition.z - BrownPosition.z) < tolerance)
-        //{
-               //return true;
-         // }
+        if ((BlockPosition.y >= BrownPosition.y + mBrownHeight) &&
+                 BlockPosition.x < (BrownPosition.x + xconstraint1 ) && BlockPosition.x > (BrownPosition.x - xconstraint2) &&
+                 BlockPosition.z < (BrownPosition.z + zconstraint1 ) && BlockPosition.z > (BrownPosition.z - zconstraint2 ))
+        {
+            return true;
+        }
 
-
-
-        //currently the function will only return false
         return false;
     }
     void Update()
@@ -77,13 +84,18 @@ public class Puzzle2Controller : MonoBehaviour
         {
             // Puzzle has been solved. Turn blocks white.
 
-
-            /*TODO: Change the color of the "Puzzle1Block" to white
-                    Puzzle1Block.GetComponent<Renderer>().material.color = Color.white;*/
+            //TODO: Change the color of the "Puzzle1Block" to white
+            puzzle1block.GetComponent<Renderer>().material.color = Color.white;
 
         }
 
-        // Add this script to the BrownBlock
+       // else {
+
+            //puzzle1block.GetComponent<Renderer>().material.color = blockColor;
+
+        //}
+
+        // Add this script to the all blocks
         // Test on Oculus to see if this functions so far
     }
 }
