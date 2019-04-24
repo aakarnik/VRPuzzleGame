631f8f1c48d5e76123a4abb55ab304601a39fdff
Anuya Karnik <aakarnik8@gmail.com>
2019-04-24 02:24:50 -0400

    code for object spawner, puzzle completion written (Needs testing)

--- VRPuzzle/Assets/Scripts/Object_Spawner.cs
+++ VRPuzzle/Assets/Scripts/Object_Spawner.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Object_Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // contains the object we want to spawn - for example, a key or a dialcontroller
+    public GameObject objectToSpawn;
+
+    //contains the spawner object which will be used to find where to create an instance of the "objectToSpawn"
+    public GameObject objectSpawner;
+
+    //hard coding to 4 for puzzle 2
+    //TESTING
+    public int requiredPlacements = 4;
+
+    //storing the count for how many pieces are/were in the right place
+    private int totalPlaced = 0;
+    
+    //currently, if the player dismantles the puzzle after putting the pieces
+    //in the right place, points will still be counted
+    //there will be no negative marking
+
+    void SpawnObject(int pts)
     {
-        
+        totalPlaced += pts;
+
+        if (totalPlaced > requiredPlacements)
+        {
+            GameObject newObject = Instantiate(objectToSpawn.gameObject, transform);
+            newObject.transform.Translate(objectSpawner.transform.position);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        //Testing
+        PuzzlePlacer.onPuzzleComplete += SpawnObject;
     }
+
+    private void OnDisable()
+    {
+        //Testing
+        PuzzlePlacer.onPuzzleComplete -= SpawnObject;
+    }
+
 }
--- VRPuzzle/Assets/Scripts/PuzzlePlacer.cs
+++ VRPuzzle/Assets/Scripts/PuzzlePlacer.cs
@@ -16,6 +16,15 @@ public class PuzzlePlacer : MonoBehaviour
     private float yTolerance = .05f;
     private float zTolerance = .05f;
 
+    //TESTING
+    private int update_pts_once = 1;
+    private int solvedCount = 0;
+
+    //This is the format of the type of function we want to access
+    //In this case we want to access void, int
+    public delegate void PuzzleComplete(int pts);
+    public static event PuzzleComplete onPuzzleComplete;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,9 +56,21 @@ public class PuzzlePlacer : MonoBehaviour
         if (isSolved() == true)
         {
             puzzlePlacementObj.GetComponent<Renderer>().material.color = Color.yellow;
+            solvedCount = 1;
+            //tackling the update method
+            //if it is called once per frame, then solvedCount will be updated once per frame
+            //TESTING 
+
+            if(update_pts_once == 1)
+            {
+                onPuzzleComplete?.Invoke(solvedCount);
+                update_pts_once = 0; //aimed at updating the points only once
+            }
+
         }
         else
         {
+            //TODO: Needs a logic for negative marking if the puzzle pieces move away from targets
             puzzlePlacementObj.GetComponent<Renderer>().material.color = Color.grey;
         }
         
